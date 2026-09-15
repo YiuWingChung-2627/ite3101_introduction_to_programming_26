61e29b4100aa6dd48a31b44c91679c6774cdfd41
YiuWingChung-2627 <wingyiu0906@gmail.com>
2026-09-15 01:56:53 +0000

    [skip actions] lab/lab02/ch02_t02_practice.py

--- lab/lab02/ch02_t02_practice.py
+++ lab/lab02/ch02_t02_practice.py
@@ -1,4 +1,7 @@
 # Assign your variables below, each on its own line!
+-	Set caesar to "Graham"
+-	Set praline to "John"
+-	Set viking to "Teresa"
 
 
 # Put your variables above this line, and uncomment the following 3 line of codes.
