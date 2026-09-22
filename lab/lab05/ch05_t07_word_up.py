9f48f85317d35b06232ff8374f0374e4ecddcf1e
YiuWingChung-2627 <wingyiu0906@gmail.com>
2026-09-22 02:48:22 +0000

    lab/lab05/ch05_t07_word_up.py

--- lab/lab05/ch05_t07_word_up.py
+++ lab/lab05/ch05_t07_word_up.py
@@ -4,7 +4,7 @@ original = input('Enter a word:')
 
 if len(original) > 0 and original.isalpha():
     print(original)
-    word=original
+    word=original.lower()
     first=word[0]
 else:
     print('empty')
