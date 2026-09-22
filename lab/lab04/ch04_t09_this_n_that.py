9e8958b7b58b6ab3af3ff0a8497392dc8e19e353
YiuWingChung-2627 <wingyiu0906@gmail.com>
2026-09-22 02:18:48 +0000

    lab/lab04/ch04_t09_this_n_that.py

--- lab/lab04/ch04_t09_this_n_that.py
+++ lab/lab04/ch04_t09_this_n_that.py
@@ -5,4 +5,5 @@ bool_two = False and not True or True
 bool_three = True and not (False or False)
 
 bool_four = not not True or False and not True
-bool_five = None
+
+bool_five =False or not (True and True)
