10ea21aed55d15c5c71c9353d0dfa4f7acd838f1
YiuWingChung-2627 <wingyiu0906@gmail.com>
2026-09-22 02:05:27 +0000

    [skip actions] lab/lab04/ch04_t04_how_tables_have_turned.py

--- lab/lab04/ch04_t04_how_tables_have_turned.py
+++ lab/lab04/ch04_t04_how_tables_have_turned.py
@@ -7,10 +7,10 @@ bool_one = 3 < 5  # We already did this one for you!
 bool_two = 1!=(2-1)
 
 # Make me true!
-bool_three = 10/2=5
+bool_three = 10/2==5
 
 # Make me false!
-bool_four = None
+bool_four = 20*7
 
 # Make me true!
 bool_five = None
